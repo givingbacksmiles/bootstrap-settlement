fdc2694272077e43150afd11155013c387cdcadd
Dave Norris <givingbacksmiles@gmail.com>
2014-01-09 15:29:01 -0500

    bootstrap-settlement init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapSettlement::Application.config.secret_key_base = 'c56504a5c007e3f8eb0596f76f54a58dc456b3ecf0a6493984cde7c613a4a5b13d87d5a2bc538ca28525b087886cb1c80f5028e006e5fa9f38fd1c5f2601065d'
